24ab4fb06e951da4f90c9b9db3446d43e519f884
Artur Nicpoń <nicpon.artur@gmail.com>
2022-08-30 19:46:36 +0200

    Dodadnie progress bar do strony

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-const progress = document.querySelector(".progress-bar_real-progress");
+// ------ progress bar ------
+(function () {
+    const bar = document.createElement("div");
+    const progress = document.createElement("div");
 
-progress.style.width = "80%"; 
+    bar.classList.add("progress-bar");
+    progress.classList.add("progress-bar_real-progress");
+
+    document.body.appendChild(bar).appendChild(progress);
+
+    window.addEventListener('scroll', (e) => {
+        let progress_bar = window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+        progress.style.width = `${progress_bar}%`;
+    });
+})();
+
