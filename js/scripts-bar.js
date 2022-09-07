5ac76ddd61eda7b9b11c15f47f9d8bc5bca7321b
Artur Nicpoń <nicpon.artur@gmail.com>
2022-09-07 09:16:59 +0200

    Ulepszenie paska progresu

--- js/scripts-bar.js
+++ js/scripts-bar.js
@@ -3,14 +3,19 @@
     const bar = document.createElement("div");
     const progress = document.createElement("div");
 
-    bar.classList.add("progress-bar");
-    progress.classList.add("progress-bar_real-progress");
+    bar.classList.add("background-bar");
+    progress.classList.add("progress-bar");
 
     document.body.appendChild(bar).appendChild(progress);
 
-    window.addEventListener('scroll', (e) => {
+    window.addEventListener('scroll', () => {
         let progress_bar = window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
         progress.style.width = `${progress_bar}%`;
+    });
+
+    bar.addEventListener('click', (e) => {
+        let setPosition = (document.documentElement.scrollHeight- document.documentElement.clientHeight) * (e.clientX / window.innerWidth);
+        window.scrollTo(0, setPosition.toFixed(0));
     });
 })();
 
